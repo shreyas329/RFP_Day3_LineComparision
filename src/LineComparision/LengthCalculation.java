7a59415da7bd23526b5b68c07067c98b7e313224
shreyas329 <sharmayashu1301@gmail.com>
2022-08-13 15:34:47 +0530

    Length comparision code is added.

---
+++ src/LineComparision/LengthCalculation.java
@@ -0,0 +1,34 @@
+package LineComparision;
+
+import java.util.Scanner;
+
+public class LengthCalculation {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line comparison program...!");
+		
+		int x1,x2,y1,y2;
+        float length;
+
+        Scanner sc=new Scanner(System.in);
+        
+        System.out.print("Enter x1 point: ");
+        x1=sc.nextInt();
+
+        System.out.print("Enter y1 point: ");
+        y1=sc.nextInt();
+        
+        System.out.print("Enter x2point: ");
+        x2=sc.nextInt();
+        
+        System.out.print("Enter y2 point: ");
+        y2=sc.nextInt();
+
+        length=(float) Math.sqrt((x2-x1)^2 + (y2-y1)^2);
+
+        System.out.println("Length of Lines is : "+"("+x1+","+y1+"),"+"("+x2+","+y2+")===>"+length);
+
+
+	}
+
+}
