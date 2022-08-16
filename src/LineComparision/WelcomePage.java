8328ec38e88fa195c3f76c77deb611ce30536982
Shreyas Sharma P <57483308+shreyas329@users.noreply.github.com>
2022-08-16 21:37:55 +0530

    Update WelcomePage.java

--- src/LineComparision/WelcomePage.java
+++ src/LineComparision/WelcomePage.java
@@ -3,7 +3,7 @@ package LineComparision;
 public class WelcomePage {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Welcome to Line comparision problem");
 
 	}
 
